4ec2032ac3a0ae1bec98c4117ca212fc6e3a5f5a
Erik <erik@basedonline.nl>
2018-09-15 18:47:07 +0200

    Prepering for 1.5 release

--- support-hours.php
+++ support-hours.php
@@ -8,17 +8,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://basedonline.nl
+ * @link              https://basedonline.nl
  * @since             1.0.0
  * @package           Support_Hours
  *
  * @wordpress-plugin
  * Plugin Name:       Support Hours
  * Plugin URI:        http://basedonline.nl
- * Description:       The support-hours plugin can be used to give your customers insight on the status of their pre-paid support hours.
- * Version:           1.4.2
+ * Description:       Use Support hours to give yourself and your clients insights on the status of pre-paid work.
+ * Version:           1.5
  * Author:            Erik van der Bas
- * Author URI:        http://basedonline.nl
+ * Author URI:        https://basedonline.nl
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       support-hours
