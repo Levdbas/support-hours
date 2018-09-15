7cb17501c80b7dda4f25d59975b61ebc579697e4
Erik <erik@basedonline.nl>
2018-09-15 11:29:15 +0200

    css changes

--- admin/support-hours-admin-settings.php
+++ admin/support-hours-admin-settings.php
@@ -23,7 +23,7 @@
 <div class="wrap">
   <?php  if (empty($users) || (!empty($users) && in_array($user_ID, $users))) { ?>
     <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
-    <form method="post" name="cleanup_options" action="options.php" class="settings">
+    <form method="post" name="cleanup_options" action="options.php" class="support-hours-settings">
       <?php
       settings_fields($name);
       do_settings_sections($name);
