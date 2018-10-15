29e21537ffbffdfe8b339dc84f08f84491d0b2a1
Erik <erik@basedonline.nl>
2018-10-15 10:19:03 +0200

    fix for input validation

--- assets/build/webpack.config.js
+++ assets/build/webpack.config.js
@@ -1,38 +1,41 @@
 /**
-* Assets Config file
-*/
+ * Assets Config file
+ */
 process.noDeprecation = true;
 const env = process.env.NODE_ENV;
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const rootPath = process.cwd();
-var configFile = require(path.resolve(__dirname,rootPath)+'/assets/config.json');
+var configFile = require(path.resolve(__dirname, rootPath) +
+  "/assets/config.json");
 
 const variables = {
-  browserSyncURL: configFile['browserSyncURL'],
-  browserSyncPort: configFile['browserSyncPort'],
-  themePath: path.join(rootPath, configFile['themePath']), // from root folder path/to/theme
-  distPath:   path.join(rootPath, configFile['themePath'], 'dist'), // from root folder path/to/theme
-  assetsPath: path.join(rootPath, configFile['assetsPath']), // from root folder path/to/assets
+  browserSyncURL: configFile["browserSyncURL"],
+  browserSyncPort: configFile["browserSyncPort"],
+  themePath: path.join(rootPath, configFile["themePath"]), // from root folder path/to/theme
+  distPath: path.join(rootPath, configFile["themePath"], "dist"), // from root folder path/to/theme
+  assetsPath: path.join(rootPath, configFile["assetsPath"]) // from root folder path/to/assets
 };
 
-const ExtractNormalCSS  = new ExtractTextPlugin(process.env.NODE_ENV === 'production' ? 'styles/[name].[chunkhash].css' : 'styles/[name].css');
-const ExtractCriticalCSS  = new ExtractTextPlugin('styles/critical.php');
+const ExtractNormalCSS = new ExtractTextPlugin("styles/[name].css");
 
 if (process.env.NODE_ENV === undefined) {
-  process.env.NODE_ENV = isProduction ? 'production' : 'development';
+  process.env.NODE_ENV = isProduction ? "production" : "development";
 }
 
 const config = {
   context: variables.assetsPath,
   entry: {
-    'support-hours-admin': ['./scripts/support-hours-admin.js', './styles/support-hours-admin.scss']
+    "support-hours-admin": [
+      "./scripts/support-hours-admin.js",
+      "./styles/support-hours-admin.scss"
+    ]
   },
   module: {
     rules: [
@@ -40,96 +43,94 @@ const config = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'buble-loader', options: { objectAssign: 'Object.assign' }
+          loader: "buble-loader",
+          options: { objectAssign: "Object.assign" }
         }
       },
       {
         test: /\.(css|scss|sass)$/,
         include: variables.assetsPath,
-        exclude: /critical.scss$/,
         use: ExtractNormalCSS.extract({
-          fallback: 'style-loader',
+          fallback: "style-loader",
           use: [
-            {loader: 'css-loader'},
+            { loader: "css-loader" },
             {
-              loader: 'postcss-loader', options: {
-                config:{
-                  path: __dirname,
-                },
-              },
+              loader: "postcss-loader",
+              options: {
+                config: {
+                  path: __dirname
+                }
+              }
             },
-            {loader: 'sass-loader'}
+            { loader: "sass-loader" }
           ],
-          publicPath: '../',
-        }),
+          publicPath: "../"
+        })
       },
       {
         test: /\.(ttf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
         include: variables.assetsPath,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 4096,
-          name: '[path][name].[ext]',
-        },
-      },
+          name: "[path][name].[ext]"
+        }
+      }
     ]
   },
   output: {
-    filename: 'scripts/[name].js',
+    filename: "scripts/[name].js",
     path: path.resolve(__dirname, variables.distPath)
   },
   externals: {
-    jquery: 'jQuery'
+    jquery: "jQuery"
   },
   plugins: [
     new webpack.ProvidePlugin({
-      Component: 'materialize-css/js/component.js',
-      Modal: 'materialize-css/js/modal.js',
-      $: 'jquery',
-      jQuery: 'jquery'
+      Component: "materialize-css/js/component.js",
+      Modal: "materialize-css/js/modal.js",
+      $: "jquery",
+      jQuery: "jquery"
     }),
     ExtractNormalCSS,
     new BrowserSyncPlugin({
-      host: 'localhost',
+      host: "localhost",
       proxy: variables.browserSyncURL,
       delay: 500,
       watch: true,
       watchOptions: {
         aggregateTimeout: 300,
         poll: 1000,
-        ignored: /node_modules/,
+        ignored: /node_modules/
       },
-      files: [
-        variables.themePath+'/**/*.php'
-      ],
+      files: [variables.themePath + "/**/*.php"]
     }),
-    new CopyWebpackPlugin([
-      {
-        context: variables.assetsPath+'/images',
-        from: '**/*',
-        to: process.env.NODE_ENV === 'production' ? 'images/[name].[hash].[ext]' : 'images/[name].[ext]',
-      }
-    ],{
-      ignore: [
-        '.gitkeep'
+    new CopyWebpackPlugin(
+      [
+        {
+          context: variables.assetsPath + "/images",
+          from: "**/*",
+          to: "images/[name].[ext]"
+        }
       ],
-      copyUnmodified: true,
-    }
-  ),
-]
+      {
+        ignore: [".gitkeep"],
+        copyUnmodified: true
+      }
+    )
+  ]
 };
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   config.plugins.push(
-    new UglifyJsPlugin({
-    }),
+    new UglifyJsPlugin({}),
     new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /(critical.php|\.css)$/i,
+      assetNameRegExp: /(critical.php|\.css)$/i
     }),
     new ImageminPlugin({
-      disable: process.env.NODE_ENV !== 'production',
+      disable: process.env.NODE_ENV !== "production",
       test: /\.(jpe?g|png|gif|svg)$/i
     })
   );
 }
-module.exports = config
+module.exports = config;
--- assets/scripts/workTableHelpers.js
+++ assets/scripts/workTableHelpers.js
@@ -1,29 +1,16 @@
-function workTableHelpers(  ) {
-
+function workTableHelpers() {
   // remove workFields row.
-  $('.remove-row').click(function(e){
-    $(this).parents('.repeating').fadeOut(500, function(){
-      $(this).remove();
-    });
+  $(".remove-row").click(function(e) {
+    $(this)
+      .parents(".repeating")
+      .fadeOut(500, function() {
+        $(this).remove();
+      });
   });
-  /* set date field to todays date.
-  $('.today').click(function(e){
-    var today = $('.currentDate').text();
-    e.preventDefault();
-    $(this).prev().val(today);
-  });
-  */
 
-  $('.datepicker').datepicker({
-    format: 'dd-mm-yyyy',
+  $(".datepicker").datepicker({
+    format: "dd-mm-yyyy",
     defaultDate: new Date()
   });
-
-  $('.timepicker').timepicker({
-    twelveHour : false,
-    defaultTime : '00:00'
-  });
-
-
 }
 export default workTableHelpers;
