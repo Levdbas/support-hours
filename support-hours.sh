52266bbbe56110c1b4cb97242e2356070de0ab40
Erik <erik@basedonline.nl>
2018-11-04 13:15:52 +0100

    remove public from bash file

--- support-hours.sh
+++ support-hours.sh
@@ -9,9 +9,7 @@ rm -r $release/trunk/*
 cp -R $dev/admin/ $trunk/admin/
 cp -R $dev/includes/ $trunk/includes/
 cp -R $dev/languages/ $trunk/languages/
-cp -R $dev/public/ $trunk/public/
 cp -R $dev/dist/ $trunk/dist/
-cp -R $dev/public/index.php $trunk/
 cp -R $dev/support-hours.php $trunk/
 cp -R $dev/uninstall.php $trunk/
 cp -R $dev/README.txt $trunk/
