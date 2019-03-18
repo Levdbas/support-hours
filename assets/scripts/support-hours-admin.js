f9984850e25c0fe7462e0497d1c16a0ab85bfe1e
Erik van der Bas - Laptop <erik@basedonline.nl>
2019-03-18 20:24:31 +0100

    admin.js update

--- assets/scripts/support-hours-admin.js
+++ assets/scripts/support-hours-admin.js
@@ -5,12 +5,34 @@ import repeatRow from "./repeatrow.js";
 import workTableHelpers from "./workTableHelpers.js";
 
 (function($) {
-  $(document).ready(function() {
-    timeInput();
-    repeatRow();
-    workTableHelpers();
-  });
-  $(window).load(function() {
-    progressBar();
-  });
+
+  var SupportHours = {
+    init: function() {
+      timeInput();
+      repeatRow();
+      workTableHelpers();
+    },
+    finalize: function() {
+      progressBar();
+    },
+  };
+  var UTIL = {
+    fire: function(funcname, args) {
+      var fire;
+      var namespace = SupportHours;
+      funcname = funcname === undefined ? 'init' : funcname;
+      fire = fire && namespace;
+      fire = fire && typeof namespace[funcname] === 'function';
+
+      if (fire) {
+        namespace[funcname](args);
+      }
+    },
+    loadEvents: function() {
+      // Fire common init JS
+      UTIL.fire();
+      UTIL.fire('finalize');
+    },
+  };
+  $(document).ready(UTIL.loadEvents);
 })(jQuery);
