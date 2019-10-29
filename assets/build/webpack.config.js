00420c1c496361f766d3cda7b45aefe6c5c4d391
Erik - Laptop <erik@basedonline.nl>
2019-10-29 17:02:10 +0100

    webpack fix

--- assets/build/webpack.config.js
+++ assets/build/webpack.config.js
@@ -4,7 +4,7 @@ const devMode = process.env.NODE_ENV !== 'production';
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack');
@@ -20,149 +20,149 @@ const rootPath = process.cwd();
 var userConfig = require(path.resolve(__dirname, rootPath) + '/assets/config.json');
 
 const config = merge(
-  {
-    path: {
-      theme: path.join(rootPath, userConfig['themePath']), // from root folder path/to/theme
-      dist: path.join(rootPath, userConfig['themePath'], 'dist'), // from root folder path/to/theme
-      assets: path.join(rootPath, userConfig['assetsPath']), // from root folder path/to/assets
+    {
+        path: {
+            theme: path.join(rootPath, userConfig['themePath']), // from root folder path/to/theme
+            dist: path.join(rootPath, userConfig['themePath'], 'dist'), // from root folder path/to/theme
+            assets: path.join(rootPath, userConfig['assetsPath']), // from root folder path/to/assets
+        },
     },
-  },
-  userConfig
+    userConfig,
 );
 
 const webpackConfig = {
-  context: config.path.assets,
-  entry: config.entry,
-  devtool: config.sourceMaps ? 'source-map' : false,
-  mode: env,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            babelrc: false,
-            cacheDirectory: true,
-          },
-        },
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '../',
-              sourceMap: config.sourceMaps,
+    context: config.path.assets,
+    entry: config.entry,
+    devtool: config.sourceMaps ? 'source-map' : false,
+    mode: env,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: false,
+                        cacheDirectory: true,
+                    },
+                },
             },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: config.sourceMaps,
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                            sourceMap: config.sourceMaps,
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: config.sourceMaps,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: config.sourceMaps,
+                            config: {
+                                path: __dirname + '/postcss.config.js',
+                            },
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: config.sourceMaps,
+                        },
+                    },
+                ],
             },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: config.sourceMaps,
-              config: {
-                path: __dirname + '/postcss.config.js',
-              },
+            {
+                test: /\.(ttf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
+                include: config.path.assets,
+                loader: 'url-loader',
+                options: {
+                    limit: 4096,
+                    name: '[path][name].[ext]',
+                },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: config.sourceMaps,
-            },
-          },
         ],
-      },
-      {
-        test: /\.(ttf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
-        include: config.path.assets,
-        loader: 'url-loader',
-        options: {
-          limit: 4096,
-          name: '[path][name].[ext]',
-        },
-      },
+    },
+    output: {
+        filename: 'scripts/[name].js',
+        path: path.resolve(__dirname, config.path.dist),
+        pathinfo: false,
+    },
+    externals: {
+        jquery: 'jQuery',
+    },
+    performance: { hints: false },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            proxy: config.browserSyncURL,
+            files: [config.path.theme + '/**/*.php', config.path.theme + '/**/*.twig'],
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css',
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+        }),
+        new CopyWebpackPlugin(
+            [
+                {
+                    context: config.path.assets + '/images',
+                    from: '**/*',
+                    to: 'images/[path][name].[ext]',
+                },
+            ],
+            {
+                ignore: ['.gitkeep'],
+                copyUnmodified: true,
+            },
+        ),
     ],
-  },
-  output: {
-    filename: 'scripts/[name].js',
-    path: path.resolve(__dirname, config.path.dist),
-    pathinfo: false,
-  },
-  externals: {
-    jquery: "jQuery"
-  },
-  performance: { hints: false },
-  plugins: [
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      proxy: config.browserSyncURL,
-      files: [config.path.theme + '/**/*.php', config.path.theme + '/**/*.twig'],
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles/[name].css',
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
-    new CopyWebpackPlugin(
-      [
-        {
-          context: config.path.assets + '/images',
-          from: '**/*',
-          to: 'images/[path][name].[ext]',
-        },
-      ],
-      {
-        ignore: ['.gitkeep'],
-        copyUnmodified: true,
-      }
-    ),
-  ],
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: config.sourceMaps,
-      }),
-      new ImageminPlugin({
-        bail: false, // Ignore errors on corrupted images
-        cache: true,
-        name: '[path][name].[ext]',
-        imageminOptions: {
-          // Lossless optimization with custom option
-          // Feel free to experement with options for better result for you
-          plugins: [
-            imageminGifsicle({
-              interlaced: true,
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: config.sourceMaps,
             }),
-            imageminJpegtran({
-              progressive: true,
+            new ImageminPlugin({
+                bail: false, // Ignore errors on corrupted images
+                cache: true,
+                name: '[path][name].[ext]',
+                imageminOptions: {
+                    // Lossless optimization with custom option
+                    // Feel free to experement with options for better result for you
+                    plugins: [
+                        imageminGifsicle({
+                            interlaced: true,
+                        }),
+                        imageminJpegtran({
+                            progressive: true,
+                        }),
+                        imageminOptipng({
+                            optimizationLevel: 1,
+                        }),
+                        imageminSvgo({
+                            removeViewBox: false,
+                        }),
+                    ],
+                },
             }),
-            imageminOptipng({
-              optimizationLevel: 1,
-            }),
-            imageminSvgo({
-              removeViewBox: false,
-            }),
-          ],
-        },
-      }),
-    ],
-  },
+        ],
+    },
 };
 if (process.env.NODE_ENV === 'production') {
-  webpackConfig.plugins.push(new CleanWebpackPlugin());
+    webpackConfig.plugins.push(new CleanWebpackPlugin());
 }
 module.exports = webpackConfig;
